2a07ccf3ebe5fb050639f3594259c5fde52d516e
tegarfn <tegarfitrah.na@gmail,com>
2022-03-27 15:31:44 +0800

    Posttest 1 085

--- 2009106085_Tegar_Fitrah_Naba_Atthoriq/src/posttest1/baju.java
+++ 2009106085_Tegar_Fitrah_Naba_Atthoriq/src/posttest1/baju.java
@@ -1,13 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package posttest1;
 
-/**
- *
- * @author PERSONAL
- */
 public class baju {
-    
+    String model;
+    String ukuran;
+    String warna;
+    int jumlah;
 }
