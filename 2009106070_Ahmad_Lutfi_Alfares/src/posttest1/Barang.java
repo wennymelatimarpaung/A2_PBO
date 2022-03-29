2256becbd8411cca50479dba30b436d55870089a
AhmadLutfiAlfares <ahmadlutfialfares7@gmail.com>
2022-03-29 16:05:28 +0800

    posttest1 070

---
+++ 2009106070_Ahmad_Lutfi_Alfares/src/posttest1/Barang.java
@@ -0,0 +1,21 @@
+package posttest1;
+
+
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Hp GK
+ */
+class Barang {
+    String nama_Penitip;
+    String nama_Barang;
+    String waktu_Serah;
+    int harga_Penitipan;
+    
+   
+}
