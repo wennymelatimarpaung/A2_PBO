6f9890a43ba679442fecae895fcc6268c1ba8d03
wennymelatimarpaung <melatimarpaung05@gmail.com>
2022-05-18 22:27:57 +0800

    Posttest6

---
+++ 2009106080_Wenny_melati_marpaung/src/Posttest6/implementss.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Posttest6;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class implementss implements inter{
+        @Override
+        public void bpjs() {
+            System.out.println("gunakan bpjs");
+        }
+
+        @Override
+        public void nonbpjs() {
+            System.out.println("gunakan membayaran mandiri");
+        }
+    }
+
+ 
+
+
+
